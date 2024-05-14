b9cf0fe10ee9f78fe080c93a4f3775184c2d94dd
Arpit Kumar <sg79720@gmail.com>
2024-05-14 13:32:08 +0530

    h

--- src/app/page.js
+++ src/app/page.js
@@ -9,11 +9,88 @@ import FeatureSection from "./components/client-only/featuresection/FeatureSecti
 export const metadata = {
   title: "Home Page",
   description: "Welcome to Homepage",
+ 
 };
+const keywords = [
+  "Gurukul Skills",
+  "online courses",
+  "skill development",
+  "skill enhancement",
+  "professional development",
+  "learning hub",
+  "educational empowerment",
+  "mastery workshops",
+  "expert-led courses",
+  "learning community",
+  "knowledge enrichment",
+  "competency development",
+  "vocational training",
+  "practical learning",
+  "career advancement courses",
+  "hands-on learning",
+  "personal growth courses",
+  "talent development",
+  "lifelong learning",
+  "professional skills training",
+  "industry-relevant courses",
+  "interactive learning",
+  "career skills development",
+  "practical knowledge",
+  "career enhancement",
+  "expert guidance",
+  "career skills workshops",
+  "educational enrichment",
+  "learning solutions",
+  "professional certification",
+  "expert-led workshops",
+  "knowledge enhancement",
+  "skill-based education",
+  "industry-specific training",
+  "learning experience",
+  "learning modules",
+  "career skills advancement",
+  "knowledge expansion",
+  "talent enrichment",
+  "practical training",
+  "professional learning",
+  "practical skill development",
+  "career skills mastery",
+  "industry skills training",
+  "career-focused training",
+  "professional skillsets",
+  "career skills empowerment",
+  "expert-led seminars",
+  "career skills development",
+  "expert-led workshops",
+  "skill mastery workshops",
+  "career skills mastery",
+  "industry-focused courses",
+  "career skills seminars",
+  "career skills advancement",
+  "practical skill workshops",
+  "career skills mastery",
+  "industry skills training",
+  "practical skill acquisition",
+  "expert-led seminars",
+  "career skills empowerment",
+  "industry-specific skill development",
+  "career skills development pathways",
+  "practical skill mastery",
+  "career skills empowerment programs",
+  "expert skill-building workshops"
+];
 
+
+// Convert keywords array to a string separated by commas
+const keywordsString = keywords.join(", ");
 const HomePage = () => {
   return (
   <div className="relative">
+    <head>   <meta
+          name="keywords"
+          content={keywordsString}
+        />
+        </head>
       <Navbar />
       <div className="relative h-[50vh] md:h-[70vh] lg:h-[70vh] xl:h-[80vh] 2xl:h-[90vh] overflow-hidden mt-16">
         <video
